c1feb47d59fa7519f97642320ea599dceab7812b
BigBoyDejv <jan.rusin3038@gmail.com>
2026-04-10 01:32:53 +0200

    waitlist2

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.2tfru4de028"
+    "revision": "0.ljr3mgk3tak"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
